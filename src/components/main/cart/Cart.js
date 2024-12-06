c96ab10b37f7ecad6344db2cbe026f2385995cfc
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-12-06 11:30:36 +0000

    updated cart

--- src/components/main/cart/Cart.js
+++ src/components/main/cart/Cart.js
@@ -15,6 +15,10 @@ const Cart = () => {
     return () => console.log("Cart unmounted");
   })
 
+  const increaseQuantity = (currentQuantity) => {
+
+  }
+
   return (
     <div className="p-4 mt-4">
       <h2 className="text-2xl font-semibold mb-7">Your Cart</h2>
@@ -38,7 +42,7 @@ const Cart = () => {
                   <p className="text-gray-700">Price: ${item.price.toFixed(2)}</p>
                   <p className="text-gray-700">Quantity: {item.quantity}</p>
                   <div className="mt-4">
-                <p className="font-semibold">Selected Customizations:</p>
+                { item.selectedOptions.length > 0 && <p className="font-semibold">Selected Customizations:</p>} 
                 <ul className="list-disc ml-6">
                   {item.selectedOptions.map((opt) => (
                     <li key={opt}>{opt}</li>
@@ -47,16 +51,35 @@ const Cart = () => {
               </div>
                 </div>
                 <div>
+                
+                <button
+                onClick={() => updateCartItem(item.id, 0)}
+                className="bg-red-500 text-white px-4 py-2 rounded"
+              >
+                Remove Item
+              </button>
+                  
+                  <span className="px-4 cursor-pointer" onClick={() =>
+                    { 
+                      if (item.quantity === 1) {
+                        return;
+                      }
+                      updateCartItem(item.id, item.quantity - 1)
+                    }
+                    } >-</span>
                   <input
-                    type="number"
                     min="1"
                     value={item.quantity}
-                    onChange={(e) =>
-                      updateCartItem(item.id, parseInt(e.target.value, 10))
+                    readOnly
+                    onChange={(e) => 
+                      updateCartItem(item.id, parseInt(e.target.value, 10)
+                    ) 
                     }
                     className="w-16 border text-center"
                   />
+                  <span className="px-4 cursor-pointer" onClick={() => updateCartItem(item.id, item.quantity + 1)}>+</span>
                 </div>
+              
               </li>
             ))}
           </ul>
