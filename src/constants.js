304f692cac0e373829e676419bf3a578b6bfcef2
StormGear <papakofiboahen@gmail.com>
2025-01-06 17:37:30 +0000

    updated baseUrl

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
-const baseUrl = "https://food-service-d1ed0096c526.herokuapp.com";
+// const baseUrl = "https://food-service-d1ed0096c526.herokuapp.com"; 
+// production server on Heroku (out of service)
+const baseUrl = "https://food-service-backend-n60w.onrender.com"; // production server on Render
 
 export default baseUrl;
